7dc6c3603e62d07f1a46159c1b0fe865edaa7d39
Israel <chiagozieohaemesi202@gmail.com>
2024-08-01 16:07:53 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './public/**/*.{html,js}', // Adjust this path according to your project structure
+      ],
+    theme: {
+      extend: {
+        colors: {
+          customGreen: '#89A54D',
+          },
+      },
+    },
+    plugins: [],
+  }
+  
+  
